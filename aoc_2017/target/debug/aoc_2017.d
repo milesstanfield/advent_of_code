7c2a691f09b18dbadeba606215b75d739e982962
Miles Stanfield <milesstanfield@gmail.com>
2023-11-21 20:35:34 -0500

    refactored folder org. did part 2 of day 1 2017

---
+++ aoc_2017/target/debug/aoc_2017.d
@@ -0,0 +1,1 @@
+/home/milesstanfield/code/advent_of_code/aoc_2017/target/debug/aoc_2017: /home/milesstanfield/code/advent_of_code/aoc_2017/src/main.rs
---
+++ aoc_2017/target/debug/deps/aoc_2017-d8f9f2a05637b17d.d
@@ -0,0 +1,5 @@
+/home/milesstanfield/code/advent_of_code/aoc_2017/target/debug/deps/aoc_2017-d8f9f2a05637b17d: src/main.rs
+
+/home/milesstanfield/code/advent_of_code/aoc_2017/target/debug/deps/aoc_2017-d8f9f2a05637b17d.d: src/main.rs
+
+src/main.rs:
