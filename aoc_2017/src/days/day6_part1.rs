2879ac73ae8ace7290f2a621ede8e3b54a79f88b
Miles Stanfield <milesstanfield@gmail.com>
2023-11-25 11:04:42 -0500

    2017 day6 part1

---
+++ aoc_2017/src/days/day6_part1.rs
@@ -0,0 +1,53 @@
+pub fn run(input: &String) {
+    let mut banks: Vec<usize> = input
+        .split(" ")
+        .map(|str| str.parse().expect("NaN"))
+        .collect();
+    let mut cycles: Vec<String> = vec![];
+
+    loop {
+        cycles.push(joined_banks(&banks));
+        banks = cycle(banks);
+        if cycles.contains(&joined_banks(&banks)) {
+            break;
+        }
+    }
+
+    println!("{:?}", cycles.len());
+}
+
+fn joined_banks(banks: &Vec<usize>) -> String {
+    let strings: Vec<String> = banks.iter().map(|int| int.to_string()).collect();
+    strings.join(",")
+}
+
+fn cycle(mut banks: Vec<usize>) -> Vec<usize> {
+    let redistribute_pos = redistribute_pos(&banks);
+    let mut distribute = banks[redistribute_pos];
+    let mut pos = redistribute_pos;
+    banks[redistribute_pos] = 0;
+
+    while distribute > 0 {
+        if pos < banks.len() - 1 {
+            pos += 1;
+        } else {
+            pos = 0;
+        }
+        banks[pos] += 1;
+        distribute -= 1;
+    }
+
+    banks
+}
+
+fn redistribute_pos(banks: &Vec<usize>) -> usize {
+    let mut pos: usize = 0;
+
+    for (i, bank) in banks.iter().enumerate() {
+        if i != 0 && bank > &banks[pos] {
+            pos = i;
+        }
+    }
+
+    pos
+}
