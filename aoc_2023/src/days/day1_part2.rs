2245e23ee5792a0083e248ba51cde3a92b7d9a1d
Miles Stanfield <milesstanfield@gmail.com>
2023-12-02 22:25:30 -0500

    prep 2023 day3 parts

--- aoc_2023/src/days/day1_part2.rs
+++ aoc_2023/src/days/day1_part2.rs
@@ -91,18 +91,19 @@ fn marker_wrapped_number_words(input: &String) -> String {
 
 #[cfg(test)]
 mod tests {
-    //     use crate::days::day1_part2::run;
+    use crate::days::day1_part2::run;
 
-    //     #[test]
-    //     fn it_works() {
-    //         let input: String = r"two1nine
-    // eightwothree
-    // abcone2threexyz
-    // xtwone3four
-    // 4nineeightseven2
-    // zoneight234
-    // 7pqrstsixteen"
-    //             .into();
-    //         assert_eq!(run(&input), 281);
-    //     }
+    #[test]
+    fn it_works() {
+        let input: String = "\
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen"
+            .into();
+        assert_eq!(run(&input), 281);
+    }
 }
