4b1410355d2374477cad3bd6f6c77b7b42a73d38
Miles Stanfield <milesstanfield@gmail.com>
2024-12-05 03:39:49 -0500

    refactored day5part1

--- aoc_2024/days/day5_part1.rb
+++ aoc_2024/days/day5_part1.rb
@@ -1,23 +1,27 @@
 module Aoc
   class Day < DayBase
+    attr_reader :rules, :updates
+
     def run
       sections = raw.split(/\n\n/)
-      rules = to_rows(sections[0]).map { |row| row.first.split("|").map(&:to_i) }
-      updates = to_rows(sections[1]).map { |row| row.first.split(",").map(&:to_i) }
-
-      valid_updates = updates.select do |update|
-        rules.all? do |rule|
-          next(true) if update.index(rule[0]).nil? || update.index(rule[1]).nil?
-          update.index(rule[0]) < update.index(rule[1])
-        end
-      end
-
-      return 0 if valid_updates.empty?
+      @rules = to_rows(sections[0]).map { |row| row.first.split("|").map(&:to_i) }
+      @updates = to_rows(sections[1]).map { |row| row.first.split(",").map(&:to_i) }
 
       valid_updates.map do |update|
         index = (update.size / 2).to_i
         update[index]
       end.sum
+    end
+
+    private
+
+    def valid_updates
+      @valid_updates ||= updates.select do |update|
+        rules.all? do |rule|
+          next(true) if update.index(rule[0]).nil? || update.index(rule[1]).nil?
+          update.index(rule[0]) < update.index(rule[1])
+        end
+      end
     end
   end
 end
