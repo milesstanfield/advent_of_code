d9143d895529133ebc8aea48b64ea2e04178a758
Miles Stanfield <milesstanfield@gmail.com>
2024-12-05 06:18:53 -0500

    day4 part1

--- aoc_2024/days/day4_part1.rb
+++ aoc_2024/days/day4_part1.rb
@@ -1,49 +1,99 @@
+class Array
+  def at_positive_index(i)
+    self.at(i) unless i < 0
+  end
+end
+
 module Aoc
   class Day < DayBase
-    # attr_reader :pos
-
     def run
       @rows = char_split_rows
+      count = 0
 
       rows.each_with_index do |row, row_i|
-        right?(row) && (puts "found right in row #{row.join}")
-
-        left?(row) && (puts "found left in row #{row.join}")
+        row.size.times do |col_i|
+          count += 1 if up_left?(row_i, col_i)
+          count += 1 if up_right?(row_i, col_i)
+          count += 1 if down_right?(row_i, col_i)
+          count += 1 if down_left?(row_i, col_i)
+          count += 1 if right?(row_i, col_i)
+          count += 1 if left?(row_i, col_i)
+          count += 1 if down?(row_i, col_i)
+          count += 1 if up?(row_i, col_i)
+        end
       end
 
-      nil
+      count
     end
 
     private
 
-    def right?(row)
-      row.each_with_index.any? do |_, char_i|
-        row[char_i..(char_i + 3)].join == "XMAS"
-      end
+    def right?(row_i, col_i)
+      rows[row_i][col_i..(col_i + 3)].join == "XMAS"
     end
 
-    def left?(row)
-      row.each_with_index.any? do |_, char_i|
-        row[(char_i - 3)..char_i].join == "SAMX"
-      end
+    def left?(row_i, col_i)
+      rows[row_i][(col_i - 3)..col_i].join == "SAMX"
     end
 
-    def nine_oclock?(row)
-      # row.each_with_index.any? do |_, char_i|
-      #   row[(char_i - 3)..char_i].join == "SAMX"
-      # end
+    def down?(row_i, col_i)
+      columns[col_i][row_i..(row_i + 3)].join == "XMAS"
+    end
+
+    def up?(row_i, col_i)
+      columns[col_i][(row_i - 3)..row_i].join == "SAMX"
+    end
+
+    def up_left?(row_i, col_i)
+      rows.at_positive_index(row_i - 3)&.at_positive_index(col_i - 3) == "S" &&
+        rows.at_positive_index(row_i - 2)&.at_positive_index(col_i - 2) == "A" &&
+        rows.at_positive_index(row_i - 1)&.at_positive_index(col_i - 1) == "M" &&
+        rows[row_i]&.at_positive_index(col_i) == "X"
+    end
+
+    def up_right?(row_i, col_i)
+      rows.at_positive_index(row_i - 3)&.at_positive_index(col_i + 3) == "S" &&
+        rows.at_positive_index(row_i - 2)&.at_positive_index(col_i + 2) == "A" &&
+        rows.at_positive_index(row_i - 1)&.at_positive_index(col_i + 1) == "M" &&
+        rows[row_i]&.at_positive_index(col_i) == "X"
+    end
+
+    def down_right?(row_i, col_i)
+      rows[row_i + 3]&.at_positive_index(col_i + 3) == "S" &&
+        rows[row_i + 2]&.at_positive_index(col_i + 2) == "A" &&
+        rows[row_i + 1]&.at_positive_index(col_i + 1) == "M" &&
+        rows[row_i]&.at_positive_index(col_i) == "X"
+    end
+
+    def down_left?(row_i, col_i)
+      rows[row_i + 3]&.at_positive_index(col_i - 3) == "S" &&
+        rows[row_i + 2]&.at_positive_index(col_i - 2) == "A" &&
+        rows[row_i + 1]&.at_positive_index(col_i - 1) == "M" &&
+        rows[row_i]&.at_positive_index(col_i) == "X"
     end
   end
 end
 
+#   0123456789
+# 0 ....XXMAS.
+# 1 .SAMXMS...
+# 2 ...S..A...
+# 3 ..A.A.MS.X
+# 4 XMASAMX.MM
+# 5 X.....XA.A
+# 6 S.S.S.S.SS
+# 7 .A.A.A.A.A
+# 8 ..M.M.M.MM
+# 9 .X.X.XMASX
 
-# ....XXMAS.
-# .SAMXMS...
-# ...S..A...
-# ..A.A.MS.X
-# XMASAMX.MM
-# X.....XA.A
-# S.S.S.S.SS
-# .A.A.A.A.A
-# ..M.M.M.MM
-# .X.X.XMASX
+#   0123456789
+# 0 MMMSXXMASM
+# 1 MSAMXMSMSA
+# 2 AMXSXMAAMM
+# 3 MSAMASMSMX
+# 4 XMASAMXAMM
+# 5 XXAMMXXAMA
+# 6 SMSMSASXSS
+# 7 SAXAMASAAA
+# 8 MAMMMXMMMM
+# 9 MXMXAXMASX
